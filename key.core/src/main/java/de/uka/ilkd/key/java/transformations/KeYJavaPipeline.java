fd803b1afb57b82d9f810f75be69046ddcc86f50
PiisRational <gecho.gaecho@gmail.com>
2026-09-23 12:39:41 +0200

    remove universe specific constructs

--- key.core/src/main/java/de/uka/ilkd/key/java/transformations/KeYJavaPipeline.java
+++ key.core/src/main/java/de/uka/ilkd/key/java/transformations/KeYJavaPipeline.java
@@ -57,7 +57,6 @@ public class KeYJavaPipeline {
         // Below is an expensive transformation as it has to traverse down
         // to an expression level.
         p.add(new ConstantStringExpressionEvaluator(pipelineServices));
-        p.add(new UniverseMethodContractAdder(pipelineServices));
         return p;
     }
 
